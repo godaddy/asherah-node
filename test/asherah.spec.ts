ec28fcab305e265cabb2203fd30167e770f7c9bd
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-11 12:59:46 -0800

    Simplify to string only

--- test/asherah.spec.ts
+++ test/asherah.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { AsherahConfig, decrypt, decrypt_from_json_string, encrypt, encrypt_to_json_string, setup, shutdown } from '../src/asherah'
+import { AsherahConfig, decrypt, encrypt, setup, shutdown } from '../src/asherah'
 import crypto from 'crypto';
 import Benchmark = require('benchmark');
 
@@ -42,7 +42,7 @@ describe('Asherah', function () {
 
     assert(input == output)
   });
-  it('Benchmarks', function() {
+  it('Benchmark RoundTrip', function() {
     this.timeout(0);
     setup({
       KMS: 'static',
@@ -67,23 +67,87 @@ describe('Asherah', function () {
 
     const suite = new Benchmark.Suite;
     const input = Buffer.from(JSON.stringify({ key1: 'value1b', nested: { secret: crypto.randomBytes(1024).toString('base64') } }, ['nested.secret']))
-
-    // add tests
     suite.add('RoundTrip#String', function() {
-        const enc = encrypt_to_json_string('partition', input);
-        decrypt_from_json_string('partition', enc);
-    })
-    .add('RoundTrip#Object', function() {
-      const enc = encrypt('partition', input);
-      decrypt('partition', enc);
+        const enc = encrypt('partition', input);
+        decrypt('partition', enc);
     })
     .on('complete', function() {
       const fastest = suite.filter('fastest');
-      console.log('Fastest is ' + fastest.map('name') + ' with mean ' + (fastest.map('stats')[0]['mean'] * 1000).toFixed(3) + 'ms');
-      const slowest = suite.filter('slowest');
-      console.log('Slowest is ' + slowest.map('name') + ' with mean ' + (slowest.map('stats')[0]['mean'] * 1000).toFixed(3) + 'ms');
+      console.log('RoundTrip mean ' + (fastest.map('stats')[0]['mean'] * 1000).toFixed(3) + 'ms');
     })
-    // run async
+    .run({ 'async': false });
+
+    shutdown();
+  });
+  it('Benchmark Encrypt', function() {
+    this.timeout(0);
+    setup({
+      KMS: 'static',
+      Metastore: 'memory',
+      ServiceName: 'TestService',
+      ProductID: 'TestProduct',
+      Verbose: false,
+      EnableSessionCaching: true,
+      ExpireAfter: null,
+      CheckInterval: null,
+      ConnectionString: null,
+      ReplicaReadConsistency: null,
+      DynamoDBEndpoint: null,
+      DynamoDBRegion: null,
+      DynamoDBTableName: null,
+      SessionCacheMaxSize: null,
+      SessionCacheDuration: null,
+      RegionMap: null,
+      PreferredRegion: null,
+      EnableRegionSuffix: null
+    });
+
+    const suite = new Benchmark.Suite;
+    const input = Buffer.from(JSON.stringify({ key1: 'value1b', nested: { secret: crypto.randomBytes(1024).toString('base64') } }, ['nested.secret']))
+    suite.add('Encrypt#String', function() {
+        encrypt('partition', input);
+    })
+    .on('complete', function() {
+      const fastest = suite.filter('fastest');
+      console.log('Encrypt mean ' + (fastest.map('stats')[0]['mean'] * 1000).toFixed(3) + 'ms');
+    })
+    .run({ 'async': false });
+
+    shutdown();
+  });
+  it('Benchmark Decrypt', function() {
+    this.timeout(0);
+    setup({
+      KMS: 'static',
+      Metastore: 'memory',
+      ServiceName: 'TestService',
+      ProductID: 'TestProduct',
+      Verbose: false,
+      EnableSessionCaching: true,
+      ExpireAfter: null,
+      CheckInterval: null,
+      ConnectionString: null,
+      ReplicaReadConsistency: null,
+      DynamoDBEndpoint: null,
+      DynamoDBRegion: null,
+      DynamoDBTableName: null,
+      SessionCacheMaxSize: null,
+      SessionCacheDuration: null,
+      RegionMap: null,
+      PreferredRegion: null,
+      EnableRegionSuffix: null
+    });
+
+    const suite = new Benchmark.Suite;
+    const input = Buffer.from(JSON.stringify({ key1: 'value1b', nested: { secret: crypto.randomBytes(1024).toString('base64') } }, ['nested.secret']))
+    const enc = encrypt('partition', input);
+    suite.add('Decrypt#String', function() {
+        decrypt('partition', enc);
+    })
+    .on('complete', function() {
+      const fastest = suite.filter('fastest');
+      console.log('Decrypt mean ' + (fastest.map('stats')[0]['mean'] * 1000).toFixed(3) + 'ms');
+    })
     .run({ 'async': false });
 
     shutdown();
