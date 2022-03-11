4c8002ad3560a5f106017bfa57d6745e62ca07f5
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-10 17:12:09 -0800

    Remove progress spam

--- download-libasherah.sh
+++ download-libasherah.sh
@@ -2,7 +2,7 @@
 
 rm -rf binaries
 
-wget --content-disposition --directory-prefix binaries/  \
+wget --no-verbose --content-disposition --directory-prefix binaries/  \
   https://github.com/godaddy/asherah-cobhan/releases/download/current/libasherah-arm64.dylib \
   https://github.com/godaddy/asherah-cobhan/releases/download/current/libasherah-arm64.so \
   https://github.com/godaddy/asherah-cobhan/releases/download/current/libasherah-x64.dylib \
