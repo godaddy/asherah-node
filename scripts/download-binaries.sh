b9b64f0e6f5e5964b862daaa9d85b5ae37cdf198
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-22 15:33:34 -0700

    Fix path

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -15,12 +15,12 @@ if [[ "${OS}" == 'Linux' ]]; then
   if [[ ${MACHINE} == 'x86_64' ]]; then
     echo "Linux x64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.so
-    shasum -a 256 --ignore-missing -c ../dist/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
   elif [[ ${MACHINE} == 'aarch64' ]]; then
     echo "Linux arm64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.so
     shasum -a 256 libasherah-arm64.so
-    shasum -a 256 --ignore-missing -c ../dist/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
   else
     echo "Unknown CPU architecture"
     exit 1
@@ -30,11 +30,11 @@ elif [[ ${OS} == 'Darwin' ]]; then
     echo "MacOS x64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.dylib
     shasum -a 256 libasherah-x64.dylib
-    shasum -a 256 --ignore-missing -c ../dist/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
   elif [[ ${MACHINE} == 'arm64' ]]; then
     echo "MacOS arm64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.dylib
-    shasum -a 256 --ignore-missing -c ../dist/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
   else
     echo "Unknown CPU architecture"
     exit 1
