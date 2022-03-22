4a6e9bb7ce4a87a446845c8156996479d34b980c
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-22 15:48:47 -0700

    No more auto-update of sha256 sums

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -15,11 +15,11 @@ if [[ "${OS}" == 'Linux' ]]; then
   if [[ ${MACHINE} == 'x86_64' ]]; then
     echo "Linux x64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.so
-    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    shasum -a 256 --ignore-missing -c ./SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
   elif [[ ${MACHINE} == 'aarch64' ]]; then
     echo "Linux arm64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.so
-    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    shasum -a 256 --ignore-missing -c ./SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
   else
     echo "Unknown CPU architecture"
     exit 1
@@ -28,11 +28,11 @@ elif [[ ${OS} == 'Darwin' ]]; then
   if [[ ${MACHINE} == 'x86_64' ]]; then
     echo "MacOS x64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.dylib
-    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    shasum -a 256 --ignore-missing -c ./SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
   elif [[ ${MACHINE} == 'arm64' ]]; then
     echo "MacOS arm64"
     curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.dylib
-    shasum -a 256 --ignore-missing -c ../sums/SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    shasum -a 256 --ignore-missing -c ./SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
   else
     echo "Unknown CPU architecture"
     exit 1
--- scripts/update-sums.sh
+++ scripts/update-sums.sh
@@ -2,8 +2,6 @@
 
 source .asherah-version
 
-mkdir -p sums
-cd sums || exit 1
 rm -f SHA256SUMS SHA256SUMS-darwin
 curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/SHA256SUMS
 curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/SHA256SUMS-darwin
