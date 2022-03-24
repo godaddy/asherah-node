41360687df1922a00815f3adb8ab6443a2a78e63
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-03-24 08:56:47 -0700

    Attempt to fix sha checking to work on all platforms

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -14,30 +14,34 @@ MACHINE=$(uname -m)
 if [[ "${OS}" == 'Linux' ]]; then
   if [[ ${MACHINE} == 'x86_64' ]]; then
     echo "Linux x64"
-    curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.so
-    shasum -a 256 --ignore-missing -c ../SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    FILENAME=libasherah-x64.so
+    SUMS=../SHA256SUMS
   elif [[ ${MACHINE} == 'aarch64' ]]; then
     echo "Linux arm64"
-    curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.so
-    shasum -a 256 --ignore-missing -c ../SHA256SUMS || (echo 'SHA256 mismatch!' ; rm -f ./*.so ; exit 1)
+    FILENAME=libasherah-arm64.so
+    SUMS=../SHA256SUMS
   else
-    echo "Unknown CPU architecture"
+    echo "Unsupported CPU architecture"
     exit 1
   fi
 elif [[ ${OS} == 'Darwin' ]]; then
   if [[ ${MACHINE} == 'x86_64' ]]; then
     echo "MacOS x64"
-    curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-x64.dylib
-    shasum -a 256 --ignore-missing -c ../SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    FILENAME=libasherah-x64.dylib
+    SUMS=../SHA256SUMS-darwin
   elif [[ ${MACHINE} == 'arm64' ]]; then
     echo "MacOS arm64"
-    curl -s -L --fail -O --retry 999 --retry-max-time 0  https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/libasherah-arm64.dylib
-    shasum -a 256 --ignore-missing -c ../SHA256SUMS-darwin || (echo 'SHA256 mismatch!' ; rm -f ./*.dylib ; exit 1)
+    FILENAME=libasherah-arm64.dylib
+    SUMS=../SHA256SUMS-darwin
   else
-    echo "Unknown CPU architecture"
+    echo "Unsupported CPU architecture"
     exit 1
   fi
 else
-  echo "Unknown operating system"
+  echo "Unsupported operating system"
   exit 1
 fi
+
+curl -s -L --fail -O --retry 999 --retry-max-time 0 "https://github.com/godaddy/asherah-cobhan/releases/download/${ASHERAH_VERSION}/${FILENAME}"
+grep "${FILENAME}" "${SUMS}" > ./SHA256SUM
+shasum -a 256 -c ./SHA256SUM || (echo 'SHA256 mismatch!' ; rm -f ./*.so ./*.dylib ; exit 1)
