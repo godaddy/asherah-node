862bf9cc3919270aaa4d3fa1932a0dc2136daf78
Jeremiah Gowdy <jeremiah@gowdy.me>
2023-10-11 21:04:30 -0700

    Initial test multi-arch

---
+++ scripts/test-arch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#echo "Updating apt"
+#sudo apt-get update -y
+
+#echo "Installing qemu support"
+#sudo apt-get install qemu binfmt-support qemu-user-static -y
+
+#echo "Register qemu support with docker"
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+echo "Testing ${TEST_ARCH} emulation"
+docker run --rm -v "$(pwd):/build" "${TEST_ARCH}/node:bookworm" /build/runs-on-emulated.sh
