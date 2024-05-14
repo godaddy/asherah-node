a98186eecbc9a414af2dd9733b4cf3f870491622
Jeremiah Gowdy <jeremiah@gowdy.me>
2024-05-14 02:50:45 -0700

    Add support for CMake based builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-node-gyp configure && node-gyp build && mkdir -p dist/ && cp build/Release/asherah.node dist/asherah.node && cp src/asherah.d.ts dist/asherah.d.ts
+set -e
+
+# Check if USE_CMAKE is set to 1
+if [ "$USE_CMAKE" = "1" ]; then
+    # Run CMake commands
+    cmake . && make && mkdir -p dist/ && cp src/asherah.d.ts dist/asherah.d.ts
+else
+    # Run node-gyp commands
+    node-gyp configure && node-gyp build && mkdir -p dist/ && cp build/Release/asherah.node dist/asherah.node && cp src/asherah.d.ts dist/asherah.d.ts
+fi
