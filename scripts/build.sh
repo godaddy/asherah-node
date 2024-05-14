63ca990efaf234294cf44ceef21a2ceffbeb2c3a
Jeremiah Gowdy <jeremiah@gowdy.me>
2024-05-14 03:25:18 -0700

    Cleaning up CMakeLists.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+mkdir -p dist/
+
 # Check if USE_CMAKE is set to 1
 if [ "$USE_CMAKE" = "1" ]; then
     # Run CMake commands
-    cmake . && make && mkdir -p dist/ && cp src/asherah.d.ts dist/asherah.d.ts
+    cmake . && make
 else
     # Run node-gyp commands
-    node-gyp configure && node-gyp build && mkdir -p dist/ && cp build/Release/asherah.node dist/asherah.node && cp src/asherah.d.ts dist/asherah.d.ts
+    node-gyp configure && node-gyp build && cp build/Release/asherah.node dist/asherah.node
 fi
+
+cp src/asherah.d.ts dist/asherah.d.ts
