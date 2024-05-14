f8f1c8bf233755a7639b20d99f1bcaaced6bedab
Jeremiah Gowdy <jeremiah@gowdy.me>
2024-05-14 05:20:25 -0700

    Make sure build failure stops the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ mkdir -p dist/
 # Check if USE_CMAKE is set to 1
 if [ "$USE_CMAKE" = "1" ]; then
     # Run CMake commands
-    cmake . && make
+    (cmake . && make) || exit 1
 else
     # Run node-gyp commands
     node-gyp configure && node-gyp build && cp build/Release/asherah.node dist/asherah.node
